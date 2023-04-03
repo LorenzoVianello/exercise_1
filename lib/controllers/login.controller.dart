bd9b204ca5c1e9aaa9772867aa5ad8c52b0b85cf
LorenzoVianello <lorenzo.vianello01@gmail.com>
2023-04-03 18:46:11 +0200

    Added snackbars for login error

--- lib/controllers/login.controller.dart
+++ lib/controllers/login.controller.dart
@@ -3,6 +3,7 @@ import 'dart:convert';
 import 'package:dio/dio.dart';
 import 'package:exercise_1/views/list/list.page.dart';
 import 'package:exercise_1/views/login/login.page.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
 
@@ -20,24 +21,44 @@ class LoginController extends GetxController {
   LoginController({required this.tag});
 
   void login() async {
-    logging_in.value = true;
-    await Dio().getUri(Uri.parse("https://flutter.dyndns.org/auth.json")).then(
-        (result) {
-      if (result.statusCode == 200) {
-        dynamic json = jsonDecode(jsonEncode(result.data));
-        token.value = json["toekn"] as String;
-        logged.value = true;
-        resetLoginData();
-        Get.to(
-            () => ItemsListPage(
-                  tag: tag,
-                ),
-            transition: Transition.leftToRight);
-      }
-    }, onError: (e) {
-      Get.log(e.toString());
-    });
-    logging_in.value = false;
+    if (areFieldsValid()) {
+      logging_in.value = true;
+      await Dio()
+          .getUri(Uri.parse("https://flutter.dyndns.org/auth.json"))
+          .then((result) {
+        if (result.statusCode == 200) {
+          dynamic json = jsonDecode(jsonEncode(result.data));
+          token.value = json["toekn"] as String;
+          logged.value = true;
+          resetLoginData();
+          Get.to(
+              () => ItemsListPage(
+                    tag: tag,
+                  ),
+              transition: Transition.leftToRight);
+        }
+      }, onError: (e) {
+        Get.log(e.toString());
+      });
+      logging_in.value = false;
+    }
+  }
+
+  bool areFieldsValid() {
+    if (user_username.text.isEmpty) {
+      spawnSnackbar(message: "Nessun nome utente fornito!");
+      return false;
+    }
+    if (user_password.text.isEmpty) {
+      spawnSnackbar(message: "Nessuna password fornita!");
+      return false;
+    }
+    return true;
+  }
+
+  void spawnSnackbar({required String message}) {
+    Get.closeAllSnackbars();
+    Get.snackbar("Errore nel login!", message, icon: Icon(Icons.error));
   }
 
 //Clear token, set logout and navigate user to LoginPage
